02e6d1a6b762783a7eca2cfc8c0b0479242f04d6
Wes Alves <wal2.designer@gmail.com>
2020-09-16 16:57:08 -0300

    relations migrations done

--- src/database/migrations/1600281482818-CreateYeastsRecipe.ts
+++ src/database/migrations/1600281482818-CreateYeastsRecipe.ts
@@ -2,22 +2,24 @@ import { MigrationInterface, QueryRunner, Table } from "typeorm";
 
 export class CreateYeastsRecipe1600281482818 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
-    new Table({
-      name: "yeasts_recipe",
-      columns: [
-        {
-          name: "id",
-          type: "varchar",
-          isPrimary: true,
-          generationStrategy: "uuid",
-          default: "uuid_generate_v4()",
-        },
-        {
-          name: "yeast_id",
-          type: "varchar",
-        },
-      ],
-    });
+    await queryRunner.createTable(
+      new Table({
+        name: "yeasts_recipe",
+        columns: [
+          {
+            name: "id",
+            type: "varchar",
+            isPrimary: true,
+            generationStrategy: "uuid",
+            default: "uuid_generate_v4()",
+          },
+          {
+            name: "yeast_id",
+            type: "varchar",
+          },
+        ],
+      })
+    );
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
--- src/database/migrations/1600281497339-CreateHopsRecipe.ts
+++ src/database/migrations/1600281497339-CreateHopsRecipe.ts
@@ -2,34 +2,36 @@ import { MigrationInterface, QueryRunner, Table } from "typeorm";
 
 export class CreateHopsRecipe1600281497339 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
-    new Table({
-      name: "hops_recipe",
-      columns: [
-        {
-          name: "id",
-          type: "varchar",
-          isPrimary: true,
-          generationStrategy: "uuid",
-          default: "uuid_generate_v4()",
-        },
-        {
-          name: "hop_id",
-          type: "varchar",
-        },
-        {
-          name: "hop_quantity",
-          type: "decimal",
-        },
-        {
-          name: "hop_add_type",
-          type: "decimal",
-        },
-        {
-          name: "hop_add_time",
-          type: "decimal",
-        },
-      ],
-    });
+    await queryRunner.createTable(
+      new Table({
+        name: "hops_recipe",
+        columns: [
+          {
+            name: "id",
+            type: "varchar",
+            isPrimary: true,
+            generationStrategy: "uuid",
+            default: "uuid_generate_v4()",
+          },
+          {
+            name: "hop_id",
+            type: "varchar",
+          },
+          {
+            name: "hop_quantity",
+            type: "decimal",
+          },
+          {
+            name: "hop_add_type",
+            type: "decimal",
+          },
+          {
+            name: "hop_add_time",
+            type: "decimal",
+          },
+        ],
+      })
+    );
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {

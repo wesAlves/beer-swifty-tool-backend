63e5e9aec3ddb850d2a0099a24c00fe1ec7452c0
Wesley <wes.bnvnd@gmail.com>
2020-10-04 16:49:03 -0300

    cors

--- src/modules/recipes/infra/http/routes/recipes.routes.ts
+++ src/modules/recipes/infra/http/routes/recipes.routes.ts
@@ -1,7 +1,7 @@
 import { response, Router } from "express";
 import { getCustomRepository } from "typeorm";
 
-import ensureAuthenticated from "../../../../../middlewares/ensureAuthenticated";
+// import ensureAuthenticated from "../../../../../middlewares/ensureAuthenticated";
 
 import RecipeRepository from "@modules/recipes/repositories/RecipeRepository";
 import CreateRecipeService from "@modules/recipes/services/CreateRecipeService";
@@ -10,91 +10,91 @@ import DeleteRecipeService from "@modules/recipes/services/DeleteRecipeService";
 
 const recipeRoutes = Router();
 
-recipeRoutes.use(ensureAuthenticated);
+// recipeRoutes.use(ensureAuthenticated);
 
 recipeRoutes.post("/", async (request, response) => {
-	const {
-		recipe_name,
-		owner_id,
-		hops,
-		fermentables,
-		yeasts,
-		srm,
-		og,
-		fg,
-		description,
-	} = request.body;
+    const {
+        recipe_name,
+        owner_id,
+        hops,
+        fermentables,
+        yeasts,
+        srm,
+        og,
+        fg,
+        description,
+    } = request.body;
 
-	const createRecipe = new CreateRecipeService();
+    const createRecipe = new CreateRecipeService();
 
-	const recipe = await createRecipe.execute({
-		recipe_name,
-		owner_id,
-		hops,
-		fermentables,
-		yeasts,
-		srm,
-		og,
-		fg,
-		description,
-	});
+    const recipe = await createRecipe.execute({
+        recipe_name,
+        owner_id,
+        hops,
+        fermentables,
+        yeasts,
+        srm,
+        og,
+        fg,
+        description,
+    });
 
-	return response.json(recipe);
+    return response.json(recipe);
 });
 
 recipeRoutes.get("/", async (request, response) => {
-	const recipeRepository = getCustomRepository(RecipeRepository);
-	const recipe = await recipeRepository.find();
+    const recipeRepository = getCustomRepository(RecipeRepository);
+    const recipe = await recipeRepository.find();
 
-	response.json(recipe);
+    response.json(recipe);
 });
 
 recipeRoutes.get("/:id", async (request, response) => {
-	const { id } = request.params;
-	const recipeRepository = getCustomRepository(RecipeRepository);
-	const recipe = await recipeRepository.find({ id: `${id}` });
+    const { id } = request.params;
+    const recipeRepository = getCustomRepository(RecipeRepository);
+    const recipe = await recipeRepository.find({ id: `${id}` });
 
-	response.json(recipe);
+    response.json(recipe);
 });
 
 recipeRoutes.put("/:id", async (request, response) => {
-	const { id } = request.params;
-	const {
-		recipe_name,
-		hops,
-		fermentables,
-		yeasts,
-		srm,
-		og,
-		fg,
-		description,
-	} = request.body;
+    const { id } = request.params;
+    const {
+        recipe_name,
+        hops,
+        fermentables,
+        yeasts,
+        srm,
+        og,
+        fg,
+        description,
+    } = request.body;
 
-	const updateRecipe = new UpdateRecipeService();
+    const updateRecipe = new UpdateRecipeService();
 
-	const recipe = await updateRecipe.execute({
-		id,
-		recipe_name,
-		hops,
-		fermentables,
-		yeasts,
-		srm,
-		og,
-		fg,
-		description,
-	});
+    const recipe = await updateRecipe.execute({
+        id,
+        recipe_name,
+        hops,
+        fermentables,
+        yeasts,
+        srm,
+        og,
+        fg,
+        description,
+    });
 
-	return response.json(recipe);
+    return response.json(recipe);
 });
 
 recipeRoutes.delete("/:id", async (request, response) => {
-	const { id } = request.params;
+    const { id } = request.params;
 
-	const deleteRecipe = new DeleteRecipeService();
+    const deleteRecipe = new DeleteRecipeService();
 
-	await deleteRecipe.execute(id);
+    await deleteRecipe.execute(id);
 
-	return response.json({ message: "Successful deleted" });
+    return response.json({ message: "Successful deleted" });
 });
 
 export default recipeRoutes;
